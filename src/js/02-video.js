a2acc2d15c1c588398e6969e52fb96a9a8ee353e
Olexandr Beisiuk <57046480+Bolexandr@users.noreply.github.com>
2022-09-07 19:03:00 +0300

    пофікисв проблему пустого сториджа

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,15 @@ import  throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then((e) => e);
+
+/// перевірка чи є в стореджі запис який задовільняє умови запуску
+// з певного моменту
+if (localStorage.getItem("videoplayer-current-time") === null) {
+player.setCurrentTime(0.01).then((e) => e);    
+} else {
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then((e) => e);    
+}
+
 
 player.on('timeupdate', throttle( timeupd => { 
       localStorage.setItem("videoplayer-current-time", timeupd.seconds);
@@ -15,13 +23,13 @@ player.setColor('#7FFFD4').then(function(color) {
     // color was successfully set
 })
 
-player.getCurrentTime().then(function (seconds) {
-  console.log('getCurrentTime =', seconds )
-    // seconds = the current playback position
-})
-player.getTextTracks().then(function (tracks) {
-    console.log('getTextTracks =', tracks )
+// player.getCurrentTime().then(function (seconds) {
+//   console.log('getCurrentTime =', seconds )
+//     // seconds = the current playback position
+// })
+// player.getTextTracks().then(function (tracks) {
+//     console.log('getTextTracks =', tracks )
 
-    // tracks = an array of track objects
-})
+//     // tracks = an array of track objects
+// })
  
